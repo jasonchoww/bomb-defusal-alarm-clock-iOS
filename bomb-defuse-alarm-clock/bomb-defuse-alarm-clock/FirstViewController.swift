0aba89a2d6bf8190e4d2c305dc29f0b8b232b6d3
SCMACEXT-10 <SCMACEXT-10@sfs-lib-480bt-NAC.sfsu.edu>
2018-12-10 15:57:00 -0800

    created a randomizer to randomly display an order in which 3 colors should be tapped

--- bomb-defuse-alarm-clock/bomb-defuse-alarm-clock/FirstViewController.swift
+++ bomb-defuse-alarm-clock/bomb-defuse-alarm-clock/FirstViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    
+    var bombAndWires = BombAndWires()
+    
+    @IBOutlet weak var Color1: UILabel!
+    
+    @IBOutlet weak var Color2: UILabel!
+    
+    @IBOutlet weak var Color3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        Color1.text = bombAndWires.getWireColorTapOrder(index: 0)
+        Color2.text = bombAndWires.getWireColorTapOrder(index: 1)
+        Color3.text = bombAndWires.getWireColorTapOrder(index: 2)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     
+    @IBAction func ColorPatternRandomizer(_ sender: UIButton) {
+        bombAndWires.randomize()
+        Color1.text = bombAndWires.getWireColorTapOrder(index: 0)
+        Color2.text = bombAndWires.getWireColorTapOrder(index: 1)
+        Color3.text = bombAndWires.getWireColorTapOrder(index: 2)
+    }
     
-
 }
 

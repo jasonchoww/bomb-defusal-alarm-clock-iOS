8f1950ad1fcde9c5ec2b96216328736e982a601f
RCMACEXT-16 <RCMACEXT-16@RCMACEXT-16.local>
2018-12-11 18:26:51 -0800

    Included wires and wire cut gestures

--- bomb-defuse-alarm-clock/bomb-defuse-alarm-clock/FirstViewController.swift
+++ bomb-defuse-alarm-clock/bomb-defuse-alarm-clock/FirstViewController.swift
@@ -18,12 +18,47 @@ class FirstViewController: UIViewController {
     
     @IBOutlet weak var Color3: UILabel!
     
+    @IBOutlet weak var wire1: UIImageView!
+    
+    @IBOutlet weak var wire2: UIImageView!
+    
+    @IBOutlet weak var wire3: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         Color1.text = bombAndWires.getWireColorTapOrder(index: 0)
         Color2.text = bombAndWires.getWireColorTapOrder(index: 1)
         Color3.text = bombAndWires.getWireColorTapOrder(index: 2)
+        wire1.image = UIImage(named: "bluewire.png")!
+        wire2.image = UIImage(named: "redwire.png")!
+        wire3.image = UIImage(named: "greenwire.png")!
+        
+        let tapGestureRecognizer1 = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer1:)))
+        let tapGestureRecognizer2 = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer2:)))
+        let tapGestureRecognizer3 = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer3:)))
+        wire1.isUserInteractionEnabled = true
+        wire2.isUserInteractionEnabled = true
+        wire3.isUserInteractionEnabled = true
+        wire1.addGestureRecognizer(tapGestureRecognizer1)
+        wire2.addGestureRecognizer(tapGestureRecognizer2)
+        wire3.addGestureRecognizer(tapGestureRecognizer3)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer1: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer1.view as! UIImageView
+        tappedImage.image = nil
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer2: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer2.view as! UIImageView
+        tappedImage.image = nil
+    }
+    
+    @objc func imageTapped(tapGestureRecognizer3: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer3.view as! UIImageView
+        tappedImage.image = nil
     }
     
     @IBAction func ColorPatternRandomizer(_ sender: UIButton) {
@@ -32,6 +67,5 @@ class FirstViewController: UIViewController {
         Color2.text = bombAndWires.getWireColorTapOrder(index: 1)
         Color3.text = bombAndWires.getWireColorTapOrder(index: 2)
     }
-    
 }
 
